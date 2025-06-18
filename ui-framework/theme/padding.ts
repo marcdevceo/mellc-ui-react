aac39efea8f65db0f9733e40054414c370192b19
Marcus Kimber <110574854+marcdevceo@users.noreply.github.com>
2025-06-18 00:43:20 -0400

    fix padding typo

--- ui-framework/theme/padding.ts
+++ ui-framework/theme/padding.ts
@@ -11,7 +11,7 @@ export const paddingClass = {
   "5xl": "p-24",
   "6xl": "p-32",
   primary: "p-4 sm:p-8 md:p-12 lg:p-16",
-  secondary: "p-4 sm:p-10 md:p-16 lg:p20",
+  secondary: "p-4 sm:p-10 md:p-16 lg:p-20",
 };
 
 export const paddingTopClass = {
